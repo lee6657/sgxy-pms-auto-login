f79553a56610f1d5564203553726d1a37990157a
lee6657 <leejing249@gmail.com>
2026-07-31 18:27:47 +0800

    cancel login when captcha image changes

--- sgxy-pms-auto-login.meta.js
+++ sgxy-pms-auto-login.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         基建系统自动登录
 // @namespace    https://docs.scriptcat.org/
-// @version      4.1.3
+// @version      4.1.4
 // @description  自动填写账号密码、OCR识别验证码、自动点击登录，并提供悬浮配置入口
 // @author       You
 // @match        https://www.sgxy-pms.sgcc.com.cn:20443/webauth/login.html
