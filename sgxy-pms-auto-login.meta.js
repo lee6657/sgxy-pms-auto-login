04e785c88ffae86f8943563e594aa7ee455d9f27
lee6657 <leejing249@gmail.com>
2026-07-31 15:55:51 +0800

    add ScriptCat update metadata

---
+++ sgxy-pms-auto-login.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         基建系统自动登录
+// @namespace    https://github.com/lee6657/sgxy-pms-auto-login
+// @version      4.0.0
+// @description  自动填写账号密码、OCR识别验证码、自动点击登录，并支持验证码错误重试
+// @author       You
+// @match        https://www.sgxy-pms.sgcc.com.cn:20443/webauth/login.html
+// @updateURL    https://raw.githubusercontent.com/lee6657/sgxy-pms-auto-login/main/sgxy-pms-auto-login.meta.js
+// @downloadURL  https://raw.githubusercontent.com/lee6657/sgxy-pms-auto-login/main/sgxy-pms-auto-login.user.js
+// @grant        GM_xmlhttpRequest
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_registerMenuCommand
+// @connect      api.zetatechs.com
+// @run-at       document-idle
+// ==/UserScript==
